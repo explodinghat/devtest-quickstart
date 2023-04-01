4d4af145537ade84dcb87d48974651c38017c0c7
warrenm2 <35501013+explodinghat@users.noreply.github.com>
2023-04-01 10:11:23 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -25,3 +25,22 @@ resource "azurerm_dev_test_lab" "example" {
     "Automated" = "True"
   }
 }
+resource "azurerm_dev_test_policy" "example" {
+  name                = "LabVmCount"
+  policy_set_name     = "default"
+  lab_name            = azurerm_dev_test_lab.example.name
+  resource_group_name = azurerm_resource_group.devtestlab.name
+  fact_data           = ""
+  threshold           = "5"
+  evaluator_type      = "MaxValuePolicy"
+}
+resource "azurerm_dev_test_virtual_network" "example" {
+  name                = "devtestlab-${var.DEVTEST_ID}-VNET"
+  lab_name            = azurerm_dev_test_lab.example.name
+  resource_group_name = azurerm_resource_group.devtestlab.name
+
+  subnet {
+    use_public_ip_address           = "Allow"
+    use_in_virtual_machine_creation = "Allow"
+  }
+}
