b999cbfe207f048b3b2da335e503d4a4d2324c57
warrenm2 <35501013+explodinghat@users.noreply.github.com>
2023-04-02 09:17:03 +0100

    corrected name of dev test schedule

--- main.tf
+++ main.tf
@@ -35,7 +35,7 @@ resource "azurerm_dev_test_policy" "example" {
   evaluator_type      = "MaxValuePolicy"
 }
 resource "azurerm_dev_test_schedule" "example" {
-  name                = "LabVmAutoStop"
+  name                = "LabVmsShutdown"
   location            = azurerm_resource_group.devtestlab.location
   resource_group_name = azurerm_resource_group.devtestlab.name
   lab_name            = azurerm_dev_test_lab.example.name
